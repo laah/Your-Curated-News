f5e2a4d7404dbd8bf7ef09f9fc52fee6592bc8bc
Laura Wentzell Ahmad <laura.s.ahmad@gmail.com>
2018-01-30 17:57:20 -0600

    adding conection

--- public/app.js
+++ public/app.js
@@ -1,33 +1,48 @@
 //Handle Scrape button
 $("#scrape").on("click", function() {
+    $('#articleContainer').empty();
     $.ajax({
         method: "GET",
         url: "/scrape",
-    }).done(function(data) {
-        console.log(data)
-        window.location = "/"
+    }).done(function(articles) {
+        console.log('this is our array of articles!!!!', articles);
+        displayResults(articles)
     })
 });
+
+
+$("#showSaved").on("click", function() {
+  $('#articleContainer').empty();
+  $.ajax({
+      method: "GET",
+      url: "/allSaved",
+  }).done(function(articles) {
+      console.log('this is our array of articles!!!!', articles);
+      displayResults(articles)
+  })
+});
+
+$('#articleContainer').on('click', '#save', function (){
+    console.log("yeahhhh!!! it is working!!", this.name);
+    $.ajax({
+      method: "POST",
+      url: "/markAsSaved",
+      data: {
+        title: this.title,
+        link: this.name
+     }
+    })
+      .done(function(responseFromBackend) {
+        console.log('thing we go back from backend ----', responseFromBackend);
+    });
+});
+
 // function that takes in articals(JSON) and creates a table body
 function displayResults(scrapedData) {
   // First, empty the table
-  $("panel-body").empty();
-
+  $(".panel-body").empty();
   // Then, for each entry of that json...
-  scrapedData.forEach(function(scrapedData) {
-    // Append each of the animal's properties to the table
-    $("panel-body").append("<ul><li>" + title + "</li>" +
-                         "<li>" + link + "</li></ul>");
+  scrapedData.forEach(function(singleData) {
+    $("#articleContainer").append('<div class="panel panel-default"> <div class="panel-heading"> <h4 id= "title">' + singleData.title + '</h4>  <a class="article-link" target="_blank" href="'+ singleData.link +'">Link</a></div><button id="save" title="' + singleData.title + '" name="' + singleData.link + '" class="btn btn-success save">Save Article</button><div class="panel-body"><p></p><a href="link" target="_blank"></a></div></div>');
   });
 }
-
-
-// 1: On Load
-// ==========
-
-// First thing: ask the back end for json with all articals
-$.getJSON("/all", function(data) {
-  // Call our function to generate a table body
-  displayResults(data);
-});
-
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var path = require("path");
 // Require request and cheerio. This makes the scraping possible
 var cheerio = require("cheerio");
@@ -10,15 +11,25 @@ var request = require("request");
 
 // Initialize Express
 var app = express();
+app.use(express.static('public'))
 
 mongoose.Promise = Promise;
 
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main",
-}));
-app.set("view engine", "handlebars");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('public'))
+
+
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({
+//     defaultLayout: "main",
+// }));
+// app.set("view engine", "handlebars");
 
 // Database configuration
 var databaseUrl = "scraper";
@@ -31,7 +42,7 @@ console.log("\n***********************************\n" +
 
 // Database configuration
 var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var collections = ["savedData"];
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
@@ -47,40 +58,47 @@ app.get("/", function(req, res) {
 
 // Scrape data from one site and place it into the mongodb db
 app.get("/scrape", function(req, res) {
+  console.log('we hit the scrape route!!!');
+  var arrayOfArticles = []
   // Make a request for the news section of Globe and mail
   request("https://www.theglobeandmail.com/world/", function(error, response, html) {
     // Load the html body from request into cheerio
+    
+
     var $ = cheerio.load(html);
     // For each element with a "title" class
     $(".o-card__content-text").each(function(i, element) {
-      console.log("this is the element found", element.parent.parent.parent.parent.attribs.href
-        );
+      // console.log("this is the element found", element.parent.parent.parent.parent.attribs.href);
       // Save the text and href of each link enclosed in the current element
       var title = element.children[0].data;
       var link = "https://www.theglobeandmail.com" + element.parent.parent.parent.parent.attribs.href;
 
       // If this found element had both a title and a link
       if (title) {
+        // console.log('we are about to push article onto our array!!');
+        arrayOfArticles.push({title: title, link: link})
         // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
+        // db.scrapedData.insert({
+        //   title: title,
+        //   link: link
+        // },
+        // function(err, inserted) {
+        //   if (err) {
+        //     // Log the error if one is encountered during the query
+        //     console.log(err);
+        //   }
+        //   else {
+        //     // Otherwise, log the inserted data
+        //     console.log(inserted);
+        //   }
+        // });
       }
     });
+    res.json(arrayOfArticles);
   });
   // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
+  // res.json(arrayOfArticles);
+  // res.send("Scrape Complete");
 });
 
   // Retrieve data from the db (to show on browser)
@@ -99,9 +117,44 @@ app.get("/all", function(req, res) {
   });
 });
 
+app.get("/allSaved", function(req, res) {
+  console.log('we hit the route ----');
+  // Find all results from the scrapedData collection in the db
+  db.savedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+
 app.get("/home", function(req, res) {
   console.log('hit the home path ----');
   res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.post('/markAsSaved', function (req,res) {
+  console.log('this is the id of the dude we want to mark saved!!!', req.body);
+
+
+  db.savedData.insert(req.body, function(err, inserted) {
+    if (err) {
+      // Log the error if one is encountered during the query
+      console.log(err);
+    }
+    else {
+      // Otherwise, log the inserted data
+      console.log('we just saved this dude ---',inserted);
+      res.json(inserted);
+    }
+  });
+
+
 });
 
 
